41902428dcef0713bbc6168854b02a1bf68b4027
myApp <kiril.ebarev@gmail.com>
2022-10-03 15:37:58 +0500

    newGame

--- lib/screens/newGameQuantity.dart
+++ lib/screens/newGameQuantity.dart
@@ -13,7 +13,7 @@ class _NewGameQuantityState extends State<NewGameQuantity> {
   // String tempText = '';
   @override
   Widget build(BuildContext context) {
-    final argumentsQuantity =
+    final _argumentsQuantity =
         (ModalRoute.of(context)?.settings.arguments ?? 3) as int;
 
     return Material(
@@ -23,28 +23,15 @@ class _NewGameQuantityState extends State<NewGameQuantity> {
           ListView.builder(
             padding: mySecondaryScreensPadding,
             physics: const BouncingScrollPhysics(),
-            itemCount: argumentsQuantity,
+            itemCount: _argumentsQuantity,
             itemBuilder: (_, index) => Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 const SizedBox(height: 10),
-                index + 1 == argumentsQuantity
+                index + 1 == _argumentsQuantity
                     ? Column(
                         children: [
-                          TextField(
-                            style: eachThemeHeaderTextStyle,
-                            enabled: true,
-                            // textAlign: TextAlign.right,
-                            decoration: InputDecoration(
-                              // alignLabelWithHint: false,
-                              focusedBorder: myBorderStyleForAddTheme,
-                              enabledBorder: myBorderStyleForAddTheme,
-                              disabledBorder: myBorderStyleForAddTheme,
-                              border: myBorderStyleForAddTheme,
-                              hintText: 'Игрок ' + (index+1).toString(),
-                              hintStyle: eachThemeHeaderTextStyle,
-                            ),
-                          ),
+                          _newGameQuantityTextForm(index),
                           const SizedBox(height: 10),
                           ElevatedButton(
                             style: mainMenuButtonStyle,
@@ -59,25 +46,29 @@ class _NewGameQuantityState extends State<NewGameQuantity> {
                           ),
                         ],
                       )
-                    : TextField(
-                        style: eachThemeHeaderTextStyle,
-                        enabled: true,
-                        // textAlign: TextAlign.right,
-                        decoration: InputDecoration(
-                          // alignLabelWithHint: false,
-                          focusedBorder: myBorderStyleForAddTheme,
-                          enabledBorder: myBorderStyleForAddTheme,
-                          disabledBorder: myBorderStyleForAddTheme,
-                          border: myBorderStyleForAddTheme,
-                          hintText: 'Игрок ' + (index+1).toString(),
-                          hintStyle: eachThemeHeaderTextStyle,
-                        ),
-                      ),
+                    : _newGameQuantityTextForm(index),
               ],
             ),
           ),
           MyBackButton(), // separatorBuilder: (_, __) => const Divider(),
         ],
+      ),
+    );
+  }
+
+  _newGameQuantityTextForm(int index) {
+    return TextFormField(
+      style: eachThemeHeaderTextStyle,
+      enabled: true,
+      // textAlign: TextAlign.right,
+      decoration: InputDecoration(
+        // alignLabelWithHint: false,
+        focusedBorder: myBorderStyleForAddTheme,
+        enabledBorder: myBorderStyleForAddTheme,
+        disabledBorder: myBorderStyleForAddTheme,
+        border: myBorderStyleForAddTheme,
+        hintText: 'Игрок ' + (index + 1).toString(),
+        hintStyle: eachThemeHeaderTextStyle,
       ),
     );
   }
