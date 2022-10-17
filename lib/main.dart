3e513f9708389bba517aec0d8aa05f42c4ba2f16
myApp <kiril.ebarev@gmail.com>
2022-10-17 12:19:12 +0500

    change icon

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "RABBIT",
+      title: "ЗАЯЦ",
       initialRoute: '/',
       routes: {
         '/': (context) => const HomeScreen(),
